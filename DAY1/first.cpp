34d3fdb4a1d58eac974e15eb5b92d4a710b1400e
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-13 21:43:52 +0530

    satrting of CPP

---
+++ DAY1/first.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    short s=12;
+    long x=1234456;
+    float f1=12.23;
+    double d1=23.323412;
+    bool isCurrent=false;
+    char rupee='$';
+    string name="hello";
+
+     std::cout<<a<<endl;
+     std::cout<<rupee<<"\n";
+      std::cout<<isCurrent<<endl;
+      std::cout<<x<<endl;
+      std::cout<<f1<<endl;
+      std::cout<<d1<<endl;
+      std::cout<<s<<endl;
+      std::cout<<name<<endl;
+
+    return 0;
+}
