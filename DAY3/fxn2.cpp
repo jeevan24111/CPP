5114f07bc811f521bf6a5184358850ef82490e27
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-24 21:34:33 +0530

    pass by value and pass by reference is  done

--- DAY3/fxn2.cpp
+++ DAY3/fxn2.cpp
@@ -13,6 +13,18 @@ void sum(float a,int b){
     cout<<a*b;
 }
 
+void swapByValue(int a,int b){
+    //pass by value
+    int temp=a;
+    a=b;
+    b=temp;
+}
+void swapByReference(int &b,int &c){
+    //pass by value
+    int temp=b;
+    b=c;
+    c=temp;
+}
 int main(){
     //fxn overloading
 
@@ -23,7 +35,30 @@ int main(){
     sum(a,c);
     sum(a,b,c);
     sum(d,b);
+    cout<<"\n";
 
-    
-    return 0;
+    //swapping of variable
+    cout<<"Call BY VALUE"<<endl;
+
+    //1.call by variable
+     cout<<"value of A Before swapping "<<a<<endl;
+    cout<<"value of b Before swapping "<<b<<endl;
+    swapByValue(a,b);
+     cout<<"value of A After swapping  "<<a<<endl;
+   cout<<"value of b  After swapping" <<b<<endl;
+
+   //in call by value copies of values are sent to the function ,acutal values are not affected 
+   /* In function when you send value they are scope will end there only
+    to change original values you have to do pass by references
+   */
+
+   //2.pass by reference
+   cout<<"CALL BY REFERENCE"<<endl;
+   cout<<"value of B Before swapping "<<b<<endl;
+    cout<<"value of C Before swapping "<<c<<endl;
+    swapByReference(b,c);
+     cout<<"value of B After swapping  "<<b<<endl;
+   cout<<"value of  C After swapping " <<c<<endl;
+
+     return 0;
 }
