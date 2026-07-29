298acdca5e66ff491ca637ec8a58dfd25a03bd3e
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-29 19:11:02 +0530

    done some chnages due wrong wrong indexing got wrong ans. It  is fixed now

--- DAY7/MajorityElementInArray.cpp
+++ DAY7/MajorityElementInArray.cpp
@@ -1,31 +1,38 @@
 #include<iostream>
 using namespace std;
 int MajorityElement(int arr[],int n){
-   int  freq[100]={0};
+   int  freq[n]={0};
     int i;
    for(i=0;i<n;i++){
     freq[arr[i]]++;
    }
-   //get all elements frequency 0 based indexing
+//    get all elements frequency 0 based indexing
    for(i=0;i<n;i++){
     cout<<freq[i]<<" ";
    }
-   int max=arr[freq[i]];
-    for(i=0;i<n;i++){
-       if(freq[i]>max){
-        max=arr[freq[i]];
-       }
+//    cout<<max;
+//    cout<<"\n";
+   //get max element freq
+    int max=-1;
+   for(i=0;i<n;i++){
+    if(freq[i]>max){
+        max=freq[i];
+    }
    }
+ int   major=arr[freq[max]+1];
+//    cout<<arr[freq[max]+1];
+   
    cout<<"\n";
-   cout<<max;
-
+  
+return major;
 
 }
 int main(){
+        // to get majority element in the array
+    int arr[7]={1,2,2,1,3,3,2};
 
-    int arr[7]={1,2,3,1,3,3,3};
-
-    int major=MajorityElement(arr,7);
+    int major1=MajorityElement(arr,7);
+    cout<<"\n Majority Element in the array arr is : "<<major1;
 
 
     return 0;
