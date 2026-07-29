2c8493be314377c6664cf19a51a6414f8ebf74ba
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-29 18:02:01 +0530

    cpp code to push back negative elements in a given array

--- DAY7/PushBackNegative.cpp
+++ DAY7/PushBackNegative.cpp
@@ -2,6 +2,49 @@
 #include<iostream>
 using namespace std;
 
+void pushBackNegative(vector<int> &arr,int n){
+    //[-42,-42,42,0,42]
+
+    vector<int>arr1(0);
+    vector<int>arr2(0);
+    vector<int>arr3(0);
+    int i;
+    for(int i=0;i<n;i++){
+        if(arr[i]<0){
+            arr1.push_back(arr[i]);
+        }else{
+            arr2.push_back(arr[i]);
+        }
+    }
+         // cout<<"\n";
+         // for(i=0;i<arr1.size();i++){
+         //     cout<<arr1[i]<<" ";->arr1 element[-42 -42 ]
+
+         // }
+         // cout<<"\n";
+        // for(i=0;i<arr2.size();i++){  ->arr2 element[42 0 42 ]
+         //     cout<<arr2[i]<<" ";
+
+        // }
+
+        //push arr1 to arr3
+        for(i=0;i<arr1.size();i++){
+            if(arr[i]!=0){
+            arr3.push_back(arr1[i]);  //push arr1 to arr3
+            }
+    }
+        //push arr2 to arr3
+        for(i=0;i<arr2.size();i++){
+            arr3.push_back(arr2[i]); 
+
+    }
+        //FInal array[-42 -42 42 0 42 ]
+        cout<<"\n";
+        for(i=0;i<arr.size();i++){
+            cout<<arr3[i]<<" ";   
+
+        }
+        }
 int main(){
 
     vector<int>arr(5);
@@ -10,7 +53,9 @@ int main(){
      for(int i=0;i<5;i++){
         cout<<arr[i]<<" ";
     }
-
+    cout<<"\n";
+    int n=arr.size();
+    pushBackNegative(arr,n);
 
     return 0;
 }
