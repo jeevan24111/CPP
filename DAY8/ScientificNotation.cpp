ed9e7a369a61eaccd6be64e3063d7f3fa2159788
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-30 05:49:53 +0530

    scientific notations

---
+++ DAY8/ScientificNotation.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+/*  Now what is best case,worst case and average case for a code(algorithm)
+ we cant calculate time complexities in sec or anything
+ so we have scientific notation to calculate time complexity
+
+        int arr[]={1,2,3,4,5,6}
+        i have to search one element in this
+        key=1
+        for(i=0;i<n;i++{
+            if(Arr[i]==kry){
+            cout<<"yes";
+            }else{
+            cout<<"no";
+            }
+        }
+
+        int arr[]={1,2,3,4,5,6}
+        if my search element is 6 i got at last time 
+        worst->time complexity ->o(n)
+
+        int arr[]={1,2,3,4,5,6}
+        best->if my search element is 1 i got at first time only
+         time complexity ->o(1)
+
+          int arr[]={1,2,3,4,5,6}
+        average->if my search element is 3 i got at middle time 
+        time complexity ->o(n/2)=>o(n);
+*/
+}
