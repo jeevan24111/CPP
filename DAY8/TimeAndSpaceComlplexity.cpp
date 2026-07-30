455cded20324b1365f49b4dae709a43f12e9ed8b
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-30 05:45:04 +0530

    time and space complexity calculations

---
+++ DAY8/TimeAndSpaceComlplexity.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*   time complexity is measured in notation
+        those are 3,they are as following
+    */
+        //theta for average case
+        //omega for best case
+        //o for worst case
+
+     /*   for(int i=0;i<n;i*=2){
+            cout<<"hello"    -time complexity =infinite loop
+       }
+
+
+    */
+
+    //for multiplication and division o(log2n) 2 is number of increment or decrement
+
+    /* for(int i=1;i<n;i*=2){
+            cout<<"hello"    -time complexity =o(log2n);
+    }*/
+        
+       
+       
+     /*  for(int i=10;i>=0;i/=2){
+            cout<<"hello"    -time complexity =o(log2n);
+       }
+           
+       
+    */
+
+    /*  while(i!=0){
+    cout<<"hello"    ->time complexity=o(n);
+    i--;
+    
+    }
+    
+
+    for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+    cout<<"hello";      -> ->time complexity=o(n^2);
+    }
+    }
+
+     for(int i=1;i<n;i++){
+    for(int j=1;j<i*i;j++){
+    cout<<"hello";      -> ->time complexity=o(n^3);
+
+
+            i*i=1,4,9...times->o(n-1)^2
+    }       outer loop->o(n)
+           total=o(n^3);
+    }
+
+    
+     for(int i=0;i<n;i++){
+    for(int j=0;j<i*i;j++){
+    cout<<"hello";      -> ->time complexity= infinite loop
+
+
+
+    */
+
+}
